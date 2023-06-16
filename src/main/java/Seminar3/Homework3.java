ac0d6ab92d57ccc47b8832aae81d8b620c3bed3a
Ruslan Karimov <ru-ka-@mail.ru>
2023-06-16 18:59:13 +0500

    added class Homework3 in Seminar3

---
+++ src/main/java/Seminar3/Homework3.java
@@ -0,0 +1,21 @@
+package Seminar3;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Homework3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        UserInputReader inputReader = new UserInputConsoleReader(scanner);
+        UserDataFileWriter fileWriter = new UserDataFileWriterImpl();
+
+        try {
+            UserData userData = inputReader.readUserData();
+            fileWriter.writeUserData(userData);
+        } catch (UserDataException | IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
